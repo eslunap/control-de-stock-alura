ebb65ab882712a0661b0897737b17f21d55abb78
eslunap19 <estefany.luna@gmail.com>
2023-05-17 20:55:01 -0500

    Update ProductoController.java
    COMMIT  Y ROLLBACK

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -70,16 +70,39 @@ public class ProductoController {
 
     public void guardar(Map<String, String> producto) throws SQLException {
 		// TODO
+		String nombre = producto.get("NOMBRE");
+		String descripcion = producto.get("DESCRIPCION");
+		Integer cantidad = Integer.valueOf(producto.get("CANTIDAD"));
+		Integer maximoCantidad = 50;
+
 		Connection con = new ConnectionFactory().recuperaConexion();
+		con.setAutoCommit(false);
 
 		PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO "
 				+ "(nombre, descripcion, cantidad)"
 				+ " VALUES(?, ?, ?)",
 				Statement.RETURN_GENERATED_KEYS);
+		try {
+			do {
+				int cantidadParaGuardar = Math.min(cantidad, maximoCantidad);
+				ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);
+				cantidad -= maximoCantidad;
+			} while (cantidad > 0);
+			System.out.println("COMMIT");
+			con.commit();
+		}catch (Exception e){
+			System.out.println("ROLLBACK");
+			con.rollback();
+		}
+		statement.close();
+		con.close();
+	}
 
-		statement.setString(1, producto.get("NOMBRE"));
-		statement.setString(2, producto.get("DESCRIPCION"));
-		statement.setInt(3, Integer.valueOf(producto.get("CANTIDAD")));
+	private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement) throws SQLException {
+
+		statement.setString(1, nombre);
+		statement.setString(2, descripcion);
+		statement.setInt(3, cantidad);
 
 		statement.execute();
 
