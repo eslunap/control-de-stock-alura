e0d589a9bff866f2aa49fba3f6df6e32a66e7d96
eslunap19 <estefany.luna@gmail.com>
2023-05-17 22:10:00 -0500

    creación de pool de conexiones

--- src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,14 +1,27 @@
 package com.alura.jdbc.factory;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+
+    private DataSource datasource;
+
+    public ConnectionFactory(){
+        var pooledDataSource = new ComboPooledDataSource();
+        pooledDataSource.setJdbcUrl("jdbc:mysql://:3306/contro_de_stock?useSSL=false&Timezone=true&serverTimezone=UTC");
+        pooledDataSource.setUser("root");
+        pooledDataSource.setPassword("root");
+        //maximo de conexiones permitidas
+        pooledDataSource.setMaxPoolSize(10);
+
+        this.datasource = pooledDataSource;
+    }
+
     public Connection recuperaConexion() throws SQLException {
-      return    DriverManager.getConnection(
-                "jdbc:mysql://:3306/contro_de_stock?useSSL=false&Timezone=true&serverTimezone=UTC",
-                "root",
-                "root");
+      return    this.datasource.getConnection();
     }
 }
